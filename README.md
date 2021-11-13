31cdc95122ce9c77ccdbdbdd49b3cf94d316929c
Justin Butera <jbutera@ucsd.edu>
2021-11-12 17:27:55 -0800

    Work on explore part of lab WIP

--- README.md
+++ README.md
@@ -11,3 +11,8 @@
 2. Would you use an end to end test to check if a function is returning the correct output? <br>
 No, because unit tests check the output of a function is correct, while end to end tests check the UI functionality.
 
+3. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user. <br>
+Yes, because we could write a unit test to ensure that the functionality of the code that writes and sends a message correctly does so, and ensure that the recipient user correctly receives the messages.
+
+4. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+No, because we would want to test the UI instead, in order to ensure that the user cannot type in the message box once they exceed 80 characters.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,73 @@
 // unit.test.js
 
+const { test } = require('@jest/globals');
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+// test isPhoneNumber()
+test('isPhoneNumber with correct phone number (001)123-457-8902', () => {
+  expect(functions.isPhoneNumber('(001)123-457-8902')).toBe(true);
+});
+
+test('isPhoneNumber with correct phone number 436-234-1956', () => {
+  expect(functions.isPhoneNumber('436-234-1956')).toBe(true);
+});
+
+test('isPhoneNumber with incorrect phone number 3948573', () => {
+  expect(functions.isPhoneNumber('3948573')).toBe(false);
+});
+
+test('isPhoneNumber with incorrect phone number 4362-34-1956', () => {
+  expect(functions.isPhoneNumber('4362-34-1956')).toBe(false);
+});
+
+// test isEmail()
+test('isEmail with correct email test@mydomain.com', () => {
+  expect(functions.isEmail('test@mydomain.com')).toBe(true);
+});
+
+test('isEmail with correct email test.blah@emailprovider.org', () => {
+  expect(functions.isEmail('test.blah@emailprovider.org')).toBe(true);
+});
+
+test('isEmail with incorrect email @abcd.com', () => {
+  expect(functions.isEmail('@abcd.com')).toBe(false);
+});
+
+test('isEmail with incorrect email testwrong@domain.123', () => {
+  expect(functions.isEmail('testwrong@domain.123')).toBe(false);
+});
+
+// test isStrongPassword()
+test('isStrongPassword with correct password ', () => {
+    expect(functions.isEmail('')).toBe(true);
+  });
+test('isStrongPassword with correct password ', () => {
+    expect(functions.isEmail('')).toBe(true);
+  });
+
+test('isStrongPassword with correct password ', () => {
+    expect(functions.isEmail('')).toBe(false);
+});
+
+test('isStrongPassword with correct password ', () => {
+    expect(functions.isEmail('')).toBe(false);
+});
+
+// test isDate()
+test('isDate with correct date ', () => {
+  expect(functions.isDate('')).toBe(true);
+});
+
+test('isDate with correct date ', () => {
+  expect(functions.isDate('')).toBe(true);
+});
+
+test('isDate with incorrect date ', () => {
+  expect(functions.isDate('')).toBe(true);
+});
+
+test('isDate with incorrect date ', () => {
+  expect(functions.isDate('')).toBe(true);
+});
